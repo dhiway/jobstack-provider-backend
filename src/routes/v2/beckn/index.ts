77c14f3835a46ad3fe4b6076214587d664a14d59
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2025-09-23 15:57:58 +0530

    feat: added select call to beckn v2

--- src/routes/v2/beckn/index.ts
+++ src/routes/v2/beckn/index.ts
@@ -1,64 +1,49 @@
 import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 import { initJobApplication } from './init';
+import {
+  InitJobApplicationSchema,
+  SelectJobApplicationSchema,
+} from './validation/request';
+import {
+  InitJobApplicationResponseSchema,
+  SelectJobApplicationResponseSchema,
+} from './validation/response';
+import { ErrorResponseSchema } from '@validation/schema/response';
+import { selectJobApplication } from './select';
 
 const becknProviderRoutes: FastifyPluginAsyncZod = async function (fastify) {
   fastify.route({
     url: '/init',
     method: 'POST',
+    schema: {
+      tags: ['Beckn-v2'],
+      body: InitJobApplicationSchema,
+      response: {
+        200: InitJobApplicationResponseSchema,
+        400: ErrorResponseSchema,
+        403: ErrorResponseSchema,
+        404: ErrorResponseSchema,
+        500: ErrorResponseSchema,
+      },
+    },
     handler: initJobApplication,
   });
-  /* fastify.route({ */
-  /*   url: '/select', */
-  /*   method: 'POST', */
-  /*   schema: { */
-  /*     body: SelectRequestSchema, */
-  /*     tags: ['Beckn'], */
-  /*     response: { */
-  /*       400: ErrorResponseSchema, */
-  /*       401: ErrorResponseSchema, */
-  /*     }, */
-  /*   }, */
-  /*   handler: selectJobPosting, */
-  /* }); */
-  /* fastify.route({ */
-  /*   url: '/init', */
-  /*   method: 'POST', */
-  /*   schema: { */
-  /*     /* body: InitRequestSchema, */
-  /*     tags: ['Beckn'], */
-  /*     response: { */
-  /*       400: ErrorResponseSchema, */
-  /*       401: ErrorResponseSchema, */
-  /*     }, */
-  /*   }, */
-  /*   handler: initJobPosting, */
-  /* }); */
-  /* fastify.route({ */
-  /*   url: '/confirm', */
-  /*   method: 'POST', */
-  /*   schema: { */
-  /*     body: ConfirmRequestSchema, */
-  /*     tags: ['Beckn'], */
-  /*     response: { */
-  /*       400: ErrorResponseSchema, */
-  /*       401: ErrorResponseSchema, */
-  /*     }, */
-  /*   }, */
-  /*   handler: confirmJobApplication, */
-  /* }); */
-  /* fastify.route({ */
-  /*   url: '/status', */
-  /*   method: 'POST', */
-  /*   schema: { */
-  /*     body: StatusRequestSchema, */
-  /*     tags: ['Beckn'], */
-  /*     response: { */
-  /*       400: ErrorResponseSchema, */
-  /*       401: ErrorResponseSchema, */
-  /*     }, */
-  /*   }, */
-  /*   handler: jobApplicationStatus, */
-  /* }); */
+  fastify.route({
+    url: '/select',
+    method: 'POST',
+    schema: {
+      tags: ['Beckn-v2'],
+      body: SelectJobApplicationSchema,
+      response: {
+        200: SelectJobApplicationResponseSchema,
+        400: ErrorResponseSchema,
+        403: ErrorResponseSchema,
+        404: ErrorResponseSchema,
+        500: ErrorResponseSchema,
+      },
+    },
+    handler: selectJobApplication,
+  });
 };
 
 export default becknProviderRoutes;
