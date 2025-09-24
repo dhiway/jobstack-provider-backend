a270aa8cf4a7989a3d84d869ef300d983eacd72b
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2025-09-25 02:14:40 +0530

    feat: added zod schema checks for createjobschema api

--- src/routes/v2/schema/index.ts
+++ src/routes/v2/schema/index.ts
@@ -1,12 +1,26 @@
 import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 import { createJobPostingSchema } from './createJobPostingSchema';
 
+import { CreateSchemaRequestSchema } from './validation/request';
+import { CreateJobPostingSchemaResponseSchema } from './validation/response';
+import { ErrorResponseSchema } from '@validation/schema/response';
+import { authMiddleware } from '@middleware/validateSession';
+
 const schemaProviderRoutes: FastifyPluginAsyncZod = async function (fastify) {
   fastify.route({
     url: '/',
     method: 'POST',
+    preHandler: authMiddleware,
     schema: {
       tags: ['Schema'],
+      body: CreateSchemaRequestSchema,
+      response: {
+        200: CreateJobPostingSchemaResponseSchema,
+        400: ErrorResponseSchema,
+        403: ErrorResponseSchema,
+        404: ErrorResponseSchema,
+        500: ErrorResponseSchema,
+      },
     },
     handler: createJobPostingSchema,
   });
