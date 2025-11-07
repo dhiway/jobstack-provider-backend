282301dacf1516359501587b3d10b9c4787ce9c6
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2025-11-07 06:02:15 +0530

    feat: added association control apis

---
+++ src/routes/v2/association/index.ts
@@ -0,0 +1,34 @@
+import { authMiddleware } from '@middleware/validateSession';
+import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
+import {
+  listAssociations,
+  ListAssociationsQuerySchema,
+} from './fetchAssociationList';
+import {
+  AssociationParamsSchema,
+  fetchAssociationOverview,
+} from './fetchAssociationOverview';
+
+export const associationProviderRoutes: FastifyPluginAsyncZod = async function (
+  fastify
+) {
+  fastify.route({
+    url: '/',
+    method: 'GET',
+    preHandler: authMiddleware,
+    handler: listAssociations,
+    schema: {
+      querystring: ListAssociationsQuerySchema,
+    },
+  });
+  fastify.route({
+    url: '/:slug/overview',
+    method: 'GET',
+    preHandler: authMiddleware,
+    handler: fetchAssociationOverview,
+    schema: {
+      querystring: ListAssociationsQuerySchema,
+      params: AssociationParamsSchema,
+    },
+  });
+};
